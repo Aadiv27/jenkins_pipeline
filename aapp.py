105e836a375a96237bac1879106e99f2321b65de
Aadiv Raj Pandey <rounak79707678@gmail.com>
2025-07-13 12:06:38 +0530

    chages

--- aapp.py
+++ aapp.py
@@ -2,6 +2,6 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/home')
 def home():
-	return "hello from Flask in juypter Notepad!"
+	return "hi i am aadive!"
 if __name__=='__main__':
 	app.run()
